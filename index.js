120da25882b080a1578afeb6671938a8091d445a
Adam <cloudflips321@icloud.com>
2021-12-29 14:46:28 -0500

    routes folder created, params created

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+// To Routes
+app.use('/api', require('./routes'))
+
+// Enable CORS
+app.use(cors())
+
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
