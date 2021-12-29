6a993e8e916191ef6089b2770cfba5a9e8ee5137
Adam <cloudflips321@icloud.com>
2021-12-29 14:51:17 -0500

    express limiter, apicache installed, limiter coded

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit')
 require('dotenv').config()
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
+
+// Rate Limit
+const limiter = rateLimit({
+  windowMs: 10*60*1000,
+  max: 5
+})
 
 // To Routes
 app.use('/api', require('./routes'))
